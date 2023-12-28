df11d48a6e56c7b19f4d81f516dda873ad86a860
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2023-12-28 19:07:12 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << "Total days in Year"<<endl<< 365<<endl<<"Circumference rate";
+    cout <<endl;
+    cout <<3.14159<<26535;
+    return 0;
+}
